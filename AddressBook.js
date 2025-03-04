ef8544737f32672db908af043eb7e9211e73d60b
pankaj143p <pankaj114477pankaj@gmail.com>
2025-03-04 13:42:02 +0530

    [Pankaj] added view person by city or state functionality

--- AddressBook.js
+++ AddressBook.js
@@ -85,3 +85,8 @@ class Contact {
       (contact) => contact.city === cityOrState || contact.state === cityOrState
     );
   }
+  function viewByCityOrState(cityOrState) {
+    return addressBook.filter(
+      (contact) => contact.city === cityOrState || contact.state === cityOrState
+    );
+  }
